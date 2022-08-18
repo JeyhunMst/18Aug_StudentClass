6bc19bd957a31bd28337816839b408bbe65f1515
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-08-18 15:28:01 +0400

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _18Aug_Task2.Models
+{
+    class Student
+    {
+        private static string GroupNo { get; set; }
+        private static string Fullname { get; set; }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0 && value < 170)
+                {
+                    _age = value;
+                }
+            }
+        }
+
+
+
+
+
+        public Student(string fullname, string groupno, int age)
+        {
+            if (CheckFullname(fullname) && CheckGroupNo(groupno))
+            {
+                Fullname = fullname;
+                GroupNo = groupno;
+                Age = age;
+            }
+
+        }
+        public static bool CheckGroupNo(string group)
+        {
+            if (group.Length == 4 && Char.IsUpper(group[0]))
+            {
+                int count = 0;
+                for (int i = 1; i < 4; i++)
+                {
+                    if (Char.IsDigit(group[i]))
+                    {
+                        count++;
+                    }
+                }
+                if (count == 3)
+                {
+                    return true;
+                }
+                return false;
+
+            }
+            return false;
+        }
+        public static bool CheckFullname(string fullName)
+        {
+            bool flag = false;
+
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                if (Char.IsDigit(fullName[i]))
+                {
+                    return false;
+                }
+            }
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                if (fullName[i] == ' ')
+                {
+                    flag = true;
+                    break;
+                }
+
+
+            }
+            if (flag == true)
+            {
+                string[] nameAndSur = fullName.Split();
+
+                //for (int i = 0; i < nameAndSur.Length; i++)
+                //{
+                //    string value = nameAndSur[i];
+                //    StringBuilder word = new StringBuilder();
+                //    word.Append(Char.ToUpper(value[0]));
+                //    word.Append(value.Substring(1).ToLower());
+                //    value = word.ToString();
+                //    nameAndSur[i] = value;
+
+
+                //}
+                int count = 0;
+                for (int i = 0; i < nameAndSur.Length; i++)
+                {
+                    string value = nameAndSur[i];
+                    if (Char.IsUpper(value[0]))
+                    {
+                        count++;
+                    }
+                }
+                if (count == nameAndSur.Length - 1)
+                {
+                    return true;
+                }
+                else
+                    return false;
+
+
+
+            }
+            else
+                return false;
+            //for (int i = 0; i < fullName.Length; i++)
+            //{
+            //    if (Char.IsDigit(fullName[i]))
+            //    {
+            //        return false;
+            //    }           
+            //}
+            //for (int i = 0; i < fullName.Length; i++)
+            //{
+            //    if (fullName[i] == ' ')
+            //    {
+            //        break;
+            //    }
+            //    else
+            //        return false;
+
+            //}
+            //if (Char.IsUpper(fullName[0]))
+            //{
+            //    int indexOf = fullName.IndexOf(' ');
+            //    if (Char.IsUpper(fullName[indexOf + 1]))
+            //    {
+            //        return true;
+            //    }
+
+            //}
+            //return false;
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        public void ShowInfo()
+            {
+                Console.WriteLine($"Name and Surname:{Fullname} \n Group number:{GroupNo} \n Age:{Age}");
+            }
+
+        }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using _18Aug_Task2.Models;
+using System;
+
+namespace _18Aug_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Enter name and surname:");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Enter group id:");
+            string groupNo = Console.ReadLine();
+            Console.WriteLine("Enter age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Student student = new Student(fullName, groupNo, age);
+            if (Student.CheckFullname(fullName) && Student.CheckGroupNo(groupNo) && age != 0)
+            {
+                student.ShowInfo();
+            }
+            else 
+            {
+                Console.WriteLine("Wrong input!!!");
+                Menu();
+            }
+                
+
+        }
+       
+
+
+    }
+}
